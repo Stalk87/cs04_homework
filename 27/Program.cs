c13bdae1392ce66e12ad9493e18a5638afbf8320
Arthur Patrakhaev <redred05@mail.ru>
2022-09-13 21:40:14 +0300

    initial commit

---
+++ 27/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+int GetResult(int number)
+{
+    int result = 0;
+    int num = number;
+    while (num > 0)
+    {
+        result = result + (num % 10);
+        num = num / 10;
+    }
+    return result;
+}
+
+Console.WriteLine($"Сумма всех чисел числа {number} = {GetResult(number)}");
---
+++ 29/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 29: Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+using System;
+
+int[] InputArrayElements()
+{
+    Console.WriteLine("Введи элементы массива через пробел");
+    string elements = Console.ReadLine()!;
+    string[] elementsSplited = elements.Split(' ');
+    int[] array = new int[elementsSplited.Length];
+
+    for (int i = 0; i < elementsSplited.Length; i++)
+    {
+        array[i] = int.Parse(elementsSplited[i]);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("Ваш массив : ");
+    foreach (int n in array)
+    {
+        Console.Write($"{n} ");
+    }
+}
+
+PrintArray(InputArrayElements());
